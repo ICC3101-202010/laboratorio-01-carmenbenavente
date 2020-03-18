24fda9561080f50f090f9e7d3bb1e43d733062ff
Carmen Benavente <cbenavente@miuandes.cl>
2020-03-18 10:37:11 -0300

    class Person

---
+++ laboratorio 1/laboratorio 1/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio_1
+{
+    class Person
+    {
+        public void Throw();
+        int values = Random.Next(0, 3);
+    }
+}
---
+++ laboratorio 1/laboratorio 1/Random.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio_1
+{
+    class Random
+    {
+        public void Next(0,3);
+
+        internal static int Next(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
